061c2546312b08f2d81f88f7d2ea23ca0d86228a
rwatriga <rwatriga@etudiant.unimes.fr>
2009-10-19 12:42:27 +0000

    ajout graphe orienté valué par des entiers ( OrientedIntValueGraph ) et FloydWarshall

--- algo/src/algo/FloydWarshall.java
+++ algo/src/algo/FloydWarshall.java
@@ -2,13 +2,145 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package algo;
 
 /**
- *
+ * algorithme de Floyd Warshall
  * @author remi
  */
 public class FloydWarshall {
 
+    //représente une distance infinie
+    public int INFINITY;
+
+    //matrice delta des chemins les plus courts
+    private int[][] delta;
+
+    //matrice du routage :
+    //P[i][j] contient le sommet précédent j sur un plus court chemin de i à j
+    private int[][] P;
+    OrientedIntValuedGraph G;
+
+    /**
+     * constructeur par défaut
+     */
+    public FloydWarshall() {
+        delta = new int[0][0];
+        P = new int[0][0];
+        G = new OrientedIntValuedGraph();
+        INFINITY = OrientedIntValuedGraph.noValue();
+    }
+
+    /**
+     * constructeur par valeur
+     * @param G un graphe orienté valué par des entiers
+     */
+    public FloydWarshall(OrientedIntValuedGraph G) {
+        this.G = G;
+        int numNodes = G.numNodes();
+        delta = new int[numNodes][numNodes];
+        P = new int[numNodes][numNodes];
+        INFINITY = OrientedIntValuedGraph.noValue();
+    }
+
+    /**
+     * lance l'algorithme de Floyd Warshall
+     */
+    public void runAlgorithm() {
+
+        //initialisation de delta et P
+        for (int i = 0; i < G.numNodes(); i++) {
+            for (int j = 0; j < G.numNodes(); j++) {
+                if (!G.get(i, j).equals(G.noValue())) {
+                    P[i][j] = i;
+                    delta[i][j] = G.get(i, j);
+                } else {
+                    P[i][j] = INFINITY;
+                    delta[i][j] = INFINITY;
+                }
+            }
+        }
+
+        //algorithme
+        for (int k = 0; k < G.numNodes(); k++) {
+            for (int j = 0; j < G.numNodes(); j++) {
+                for (int i = 0; i < G.numNodes(); i++) {
+                    if (delta[i][k] + delta[k][j] < delta[i][j]) {
+                        P[i][j] = P[k][j];
+                    }
+                    delta[i][j] = min(delta[i][j], delta[i][k], delta[k][j]);
+                }
+            }
+        }
+    }
+
+    /**
+     * affiche la matrice delta
+     */
+    public void showDelta() {
+        try {
+
+            if (delta == null) {
+                throw new NullPointerException("FloydWarshall::showP : delta==null");
+            } else {
+
+                String str = "";
+                int T = delta.length;
+                for (int i = 0; i < T; i++) {
+                    for (int j = 0; j < T; j++) {
+                        str += delta[i][j] + "\t";
+                    }
+                    str += "\n";
+                }
+
+                System.out.println(str.replace(G.noValue().toString(), "N"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * affiche la matrice P
+     */
+    public void showP() {
+        try {
+
+            if (P == null) {
+                throw new NullPointerException("FloydWarshall::showP : P==null");
+            } else {
+
+                String str = "";
+                int T = P.length;
+                for (int i = 0; i < T; i++) {
+                    for (int j = 0; j < T; j++) {
+                        str += P[i][j] + "\t";
+                    }
+                    str += "\n";
+                }
+
+                System.out.println(str.replace(OrientedIntValuedGraph.noValue().toString(), "N"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * retourne le minimum entre a et b+c
+     * en prenant en compte les cas où b ou c = infini ( infini-x = infini )
+     * @param a
+     * @param b
+     * @param c
+     * @return min (a, b+c)
+     */
+    private int min(int a, int b, int c) {
+        if ((b == OrientedIntValuedGraph.noValue()) || (c == OrientedIntValuedGraph.noValue())) {
+            return a;
+        } else {
+            return (a < b + c ? a : b + c);
+        }
+    }
 }
--- algo/src/algo/Graph.java
+++ algo/src/algo/Graph.java
@@ -2,13 +2,100 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package algo;
 
 /**
- *
+ * représente un graphe quelconque (valué ou non, arcs labellisés par des objets Value)
  * @author remi
  */
-public class Graph {
+public abstract class Graph<Value> {
 
+    //Nombre par défaut de sommets (constructeur par défaut)
+    public static final int DEFAULT_NUM_NODES = 1000;
+
+    //matrice des labels des arcs
+    protected Value[][] value;
+
+    /**
+     * représente l'abscence de valeur
+     * @return la représentation formelle de l'abscence de valeur
+     */
+    public static Object noValue() {
+        return null;
+    }
+
+    /**
+     * constructeur par défaut
+     */
+    public Graph() {
+        value = (Value[][]) new Object[DEFAULT_NUM_NODES][DEFAULT_NUM_NODES];
+        for (int i = 0; i < DEFAULT_NUM_NODES; i++) {
+            for (int j = 0; j < DEFAULT_NUM_NODES; j++) {
+                value[i][j] = (Value) noValue();
+            }
+        }
+    }
+
+    /**
+     * constructeur par valeur;
+     * @param adj la matrice d'adjacence du graphe (matrice des labels des arcs)
+     */
+    public Graph(Value[][] value) {
+
+        //copie en profondeur
+
+        int T = value.length;
+        this.value = (Value[][]) new Object[T][T];
+
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < T; j++) {
+                this.value[i][j] = value[i][j];
+            }
+        }
+    }
+
+    /**
+     * retourne le label de l'arc (i, j)
+     * @param i sommet de départ
+     * @param j sommet d'arrivée
+     * @return le label de l'arc (i, j)
+     */
+    public Value get(int i, int j) {
+        try {
+            if ((i < 0) || (j < 0) || (i >= value.length) || (j >= value.length)) {
+                throw new ArrayIndexOutOfBoundsException("Graph::get : (" + i + ", " + j + ")");
+            } else {
+                return value[i][j];
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * modifie le label de l'arc (i, j)
+     * @param i le sommet de départ
+     * @param j le sommet d'arrivée
+     * @param v le nouveau label de l'arc (i, j)
+     */
+    public void set(int i, int j, Value v) {
+        try {
+            if ((i < 0) || (j < 0) || (i >= value.length) || (j >= value.length)) {
+                throw new ArrayIndexOutOfBoundsException("Graph::set : (" + i + ", " + j + ")");
+            } else {
+                value[i][j] = v;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * le nombre de sommets
+     * @return le nombre de sommets
+     */
+    protected int numNodes() {
+        return value.length;
+    }
 }
