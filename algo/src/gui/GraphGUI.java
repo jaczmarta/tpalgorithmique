87d7e16cb6df04b5efae15f4802ac8da1be4e1df
tholivieri <tholivieri@23570c18-bc9c-11de-8d03-27772b4b08c5>
2009-12-15 23:36:00 +0000

    Nouvelle interface

--- algo/src/gui/GraphGUI.java
+++ algo/src/gui/GraphGUI.java
@@ -1,15 +1,15 @@
 package gui;
 
+import graphs.FlowCostGraph;
+import graphs.OrientedValuedGraph;
+import graphs.RandomGraphBuilder;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.logging.Level;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -21,28 +21,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYSplineRenderer;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-
-import algo.BasicMaxFlowMinCost;
-import algo.BusackerGowen;
-
-import graphs.FlowCostGraph;
-import graphs.OrientedValuedGraph;
-import graphs.RandomGraphBuilder;
-
-public class GraphMainGUI extends Window {
-	
+public class GraphGUI extends Window {
 	private static final long serialVersionUID = 1L;
 
 	JMenuBar menuBar;
@@ -51,12 +30,7 @@ public class GraphMainGUI extends Window {
 			JMenuItem quitMenuItem;
 			
 	StringBuffer choices;
-	
-
-	int numGraphs = 1;
-	JTextField numGraphsEntry;
-	JLabel graphsLabel;
-	
+		
 	int numVertices = 10;
 	JTextField numVerticesEntry;
 	JLabel verticesLabel;
@@ -82,14 +56,10 @@ public class GraphMainGUI extends Window {
 	int maxCapacity = 5;
 	JTextField maxCapacityEntry;
 	JLabel maxCapacityLabel;
-	
-	int numTests = 1;
-	JTextField numTestsEntry;
-	JLabel numTestsLabel;
-	
+		
 	JButton createGraphButton;
     
-	public GraphMainGUI() {
+	public GraphGUI() {
         super("Générateur de graphe");
         init();
 		setVisible(true);
@@ -98,7 +68,7 @@ public class GraphMainGUI extends Window {
 	
 	
 	public void init() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setMenuBar();
 		
         GridBagConstraints grid = new GridBagConstraints();
@@ -134,36 +104,10 @@ public class GraphMainGUI extends Window {
         costSelection(grid);
         capacitySelection(grid);
         
-//        {
-//        	graphsLabel = new JLabel();    
-//        	graphsLabel.setText("Nombre de graphes à générer:");
-//			grid.gridx = 0;
-//			grid.gridy = 8;
-//			getContentPane().add(graphsLabel, grid);
-//	
-//			numGraphsEntry = new JTextField("", 5);     
-//			grid.gridx = 1;
-//			grid.gridy = 8;
-//			getContentPane().add(numGraphsEntry, grid);
-//        }
-        
-        {
-        	numTestsLabel = new JLabel();    
-        	numTestsLabel.setText("Nombre de tests à éffectuer:");
-			grid.gridx = 0;
-			grid.gridy = 9;
-			getContentPane().add(numTestsLabel, grid);
-	
-			numTestsEntry = new JTextField("", 5);     
-			grid.gridx = 1;
-			grid.gridy = 9;
-			getContentPane().add(numTestsEntry, grid);
-        }
-
         {
         	createGraphButton = new JButton("Générer !");
 			grid.gridx = 3; 
-			grid.gridy = 10; 
+			grid.gridy = 9; 
 			getContentPane().add(createGraphButton, grid);
 			createGraphButton.addActionListener(this);
         }
@@ -173,9 +117,6 @@ public class GraphMainGUI extends Window {
 	public void setMenuBar() {
 		menuBar 	= new JMenuBar();
 		fileMenu 	= new JMenu("Fichier");	
-		
-		openMenuItem = new JMenuItem("Ouvrir un graphe...");
-		quitMenuItem = new JMenuItem("Quitter");
 		
 		openMenuItem = new JMenuItem("Ouvrir un graphe...");
 		quitMenuItem = new JMenuItem("Quitter");
@@ -229,6 +170,7 @@ public class GraphMainGUI extends Window {
 			}
 		}
 		else if (source == createGraphButton) {
+	        
 			RandomGraphBuilder builder = new RandomGraphBuilder();
 			try {
 				numVertices 	= Integer.parseInt(numVerticesEntry.getText());
@@ -237,8 +179,6 @@ public class GraphMainGUI extends Window {
 				maxCost 		= Integer.parseInt(maxCostEntry.getText());
 				minCapacity 	= Integer.parseInt(minCapacityEntry.getText());
 				maxCapacity 	= Integer.parseInt(maxCapacityEntry.getText());
-				//numGraphs		= Integer.parseInt(numGraphsEntry.getText());
-				numTests		= Integer.parseInt(numTestsEntry.getText());
 				
 	            builder.setNumVertices(numVertices);
 	            builder.setDensity(density);
@@ -246,118 +186,24 @@ public class GraphMainGUI extends Window {
 	            builder.setCapacityUpperBound(maxCapacity);
 		        builder.setCostLowerBound(minCost);
 		        builder.setCostUpperBound(maxCost);
-		        	
-	        	long start;
-	            long time;
-	            double avancement = 0;
-	            HashMap<Integer, Double> basicResults = new HashMap<Integer, Double>();
-	            HashMap<Integer, Double> busackerGowenResults = new HashMap<Integer, Double>();
 
-	            for (int i = 0; i < numTests; i++)
-	            {
-	                System.out.println("\navancement : " + (i+1) + "/"+numTests);
+                FlowCostGraph G = builder.generateRandomFlowGraph();
 
-
-	                double timeBasic = 0;
-	                double timeBusackerGowen = 0;
-
-	                for (int n = 7; n < numVertices; n++)
-	                {
-	                    System.out.print(".");
-	                    avancement = ((double) ((n - 7) * 100)) / (numVertices - 7);
-
-	                    FlowCostGraph G2 = builder.generateRandomFlowGraph();
-
-	                    start = System.currentTimeMillis();
-	                    {
-
-	                        BusackerGowen bg = new BusackerGowen(G2);
-	                        bg.runAlgorithm();
-	                    }
-	                    time = System.currentTimeMillis() - start;
-
-	                    timeBusackerGowen += time;
-
-
-	                    FlowCostGraph G1 = builder.generateRandomFlowGraph();
-
-	                    start = System.currentTimeMillis();
-	                    {
-	                        BasicMaxFlowMinCost bmfmc = new BasicMaxFlowMinCost(G1);
-	                        bmfmc.runAlgorithm();
-	                    }
-	                    time = System.currentTimeMillis() - start;
-
-	                    timeBasic += time;
-
-	                    if (i == 0)
-	                    {
-	                        basicResults.put(n, timeBasic);
-	                        busackerGowenResults.put(n, timeBusackerGowen);
-	                    } else
-	                    {
-	                        basicResults.put(n, basicResults.get(n) + timeBasic);
-	                        busackerGowenResults.put(n, busackerGowenResults.get(n) + timeBusackerGowen);
-	                    }
-
-
-	                }
-	                for (int n = 7; n < numVertices; n++)
-	                {
-	                    basicResults.put(n, basicResults.get(n) / numTests);
-	                    busackerGowenResults.put(n, busackerGowenResults.get(n) / numTests);
-	                }
-	               
-	            }
-                displayGraphic(basicResults, busackerGowenResults);
-	        }
-		
-			catch (NumberFormatException exception) {
-				JOptionPane.showMessageDialog(this,
-					       "Tous les champs n'ont pas été remplis correctement.",
-					       "Warning", 
-					        JOptionPane.WARNING_MESSAGE);
+                new FlowCostGraphWindow(G);
+        
+	        
 			}
-			catch (Exception e) {
-				new ErrorMessage(e);
-			}
+            catch (NumberFormatException exception) {
+                    JOptionPane.showMessageDialog(this,
+                                   "Tous les champs n'ont pas été remplis correctement.",
+                                   "Warning", 
+                                    JOptionPane.WARNING_MESSAGE);
+            }
+            catch (Exception e) {
+                    new ErrorMessage(e);
+            }
 		}
     }
-
-	public void displayGraphic(HashMap<Integer, Double> basic, HashMap<Integer, Double> bugow) {
-
-		XYSeriesCollection dataset = new XYSeriesCollection();
-		
-        XYSeries xy1 = new XYSeries("Basic");
-        XYSeries xy2 = new XYSeries("Busacker & Gowen");
-        
-	    for (int n = 7; n < numVertices; n++)
-        {
-	    	xy1.add(n, basic.get(n));
-	    	xy2.add(n, bugow.get(n));
-        }
-
-        dataset.addSeries(xy1);
-        dataset.addSeries(xy2);
-	    
-	    JFreeChart chart = ChartFactory.createXYLineChart(
-	    		"Graphe ",
-	    		"Nombre de sommets",
-	    		"Temps (en secondes)",
-	    		dataset,
-	    		PlotOrientation.VERTICAL,
-	    		true,
-	    		true,
-	    		false
-	         	);
-	    
-	    chart.getXYPlot().setRenderer(new XYSplineRenderer());
-	    
-		ChartFrame frame = new ChartFrame("Graphique", chart);
-		frame.setVisible(true);
-		frame.pack();
-		
-	}
 	
 	public void capacitySelection(GridBagConstraints grid) {
 		{
@@ -418,8 +264,4 @@ public class GraphMainGUI extends Window {
         }   
 		
 	}
-	
-	
 }
-
-
---
+++ algo/src/gui/MainGUI.java
@@ -0,0 +1,89 @@
+package gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MainGUI extends Window {
+	
+	private static final long serialVersionUID = 1L;
+
+	JMenuBar menuBar;
+		JMenu fileMenu;
+			JMenuItem quitMenuItem;
+	
+	JButton generateGraphButton;
+	JButton testTheMethodsButton;
+    
+	public MainGUI() {
+        super("Graphes & co");
+        init();
+		setVisible(true);
+		pack();
+	}
+	
+	
+	public void init() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setMenuBar();
+		
+        GridBagConstraints grid = new GridBagConstraints();
+        getContentPane().setLayout(new GridBagLayout());        
+        grid.insets = new Insets(5,5,5,5);  //top padding
+               
+        
+        {
+        	generateGraphButton = new JButton("Générer un graphe");
+			grid.gridx = 0; 
+			grid.gridy = 0; 
+			getContentPane().add(generateGraphButton, grid);
+			generateGraphButton.addActionListener(this);
+        }
+        
+        {
+        	testTheMethodsButton = new JButton("Comparer les méthodes");
+			grid.gridx = 0; 
+			grid.gridy = 1; 
+			getContentPane().add(testTheMethodsButton, grid);
+			testTheMethodsButton.addActionListener(this);
+        }
+        
+	}
+	
+	public void setMenuBar() {
+		menuBar 	= new JMenuBar();
+		fileMenu 	= new JMenu("Fichier");	
+		
+		quitMenuItem = new JMenuItem("Quitter");
+
+		fileMenu.add(quitMenuItem);								
+		menuBar	.add(fileMenu);
+
+		quitMenuItem.addActionListener(this);
+
+		menuBar	.setVisible(true);
+		setJMenuBar(menuBar);
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+		Object source = evt.getSource();
+		if (source == quitMenuItem) {
+			System.exit(1);
+		}
+		else if (source == generateGraphButton) {
+	        new GraphGUI();
+		}
+		else if (source == testTheMethodsButton) {
+	        new TestGUI();
+		}
+    }
+}
+
+
